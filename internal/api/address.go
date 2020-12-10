545a1fbe3bfb2233533d097f4d1e9b65d75b11f4
fianine <princealfiyana@gmail.com>
2020-12-10 17:04:23 +0700

    Update & Delete

--- internal/api/address.go
+++ internal/api/address.go
@@ -100,3 +100,63 @@ func AddUserAddress(w http.ResponseWriter, r *http.Request) {
 
 	responseWithJson(w, response)
 }
+
+func UpdateUserAddress(w http.ResponseWriter, r *http.Request) {
+	connection, err := config.ConnectSQL()
+	defer connection.SQL.Close()
+
+	parseErr := r.ParseForm()
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	userID := r.Form.Get("user_id")
+	address := r.Form.Get("address")
+	city := r.Form.Get("city")
+	province := r.Form.Get("province")
+
+	_, err = connection.SQL.Exec("UPDATE addresses SET address = ?, city = ?, province = ? WHERE user_id = ?",
+		address,
+		city,
+		province,
+		userID,
+	)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	response := model.Response{
+		Status:  201,
+		Message: "Success",
+	}
+
+	responseWithJson(w, response)
+}
+
+func DeleteUserAddress(w http.ResponseWriter, r *http.Request) {
+	connection, err := config.ConnectSQL()
+	defer connection.SQL.Close()
+
+	parseErr := r.ParseForm()
+	if parseErr != nil {
+		panic(parseErr)
+	}
+
+	userID := r.Form.Get("user_id")
+
+	_, err = connection.SQL.Exec("DELETE from addresses WHERE user_id = ?",
+		userID,
+	)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	response := model.Response{
+		Status:  201,
+		Message: "Success",
+	}
+
+	responseWithJson(w, response)
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -14,6 +14,8 @@ func Router() *mux.Router {
 	router.HandleFunc("/addresses", api.GetAddress).Methods("GET")
 	router.HandleFunc("/user_address", api.UserAddress).Methods("GET")
 	router.HandleFunc("/add_user_address", api.AddUserAddress).Methods("POST")
+	router.HandleFunc("/update_user_address", api.UpdateUserAddress).Methods("POST")
+	router.HandleFunc("/delete_user_address", api.DeleteUserAddress).Methods("POST")
 
 	http.Handle("/", router)
 
